09a63d4da23d9bb1c959519890dcb5acaa5fb85c
RobertoGianfelice <rob.gianfelice@gmail.com>
2020-02-07 18:26:53 +0100

    Verifica frasi palindrome

---
+++ Stringhe/palindromoFrasi.py
@@ -0,0 +1,23 @@
+stringa=input("Inserisci la stringa da analizzare: ")
+stringa=stringa.lower()
+sinistra=0
+destra=len(stringa)-1
+palindromo=True
+
+while (sinistra<destra and palindromo):
+    if (stringa[sinistra] in " ',’" ):
+        print("salto <" + stringa[sinistra] + ">")
+        sinistra+=1
+    elif (stringa[destra] in " ',’"):
+        print("salto " + stringa[destra])
+        destra-=1
+    else:
+        palindromo=stringa[sinistra]==stringa[destra]
+        print("confronto " +stringa[sinistra] + " e "+stringa[destra])
+        sinistra+=1
+        destra-=1
+
+if palindromo:
+    print("La stringa è un palindromo")
+else:
+    print("La stringa non è un palindromo")
