a6021833361b109f0254abc486260be58d3555b2
Roberto Gianfelice <rob.gianfelice@gmail.com>
2023-01-15 16:36:13 +0100

    Create Versione1.py
    Versione iterativ a forza bruta. Calla prima tutte le permutazioni usando la libreria di python e poi le testa tutte. La complessità computazionale a livello di spazio ha un grosso impatto limitando l'esecuzione sino a permutazioni di 11 elementi: oltre l'allocazione di memoria risulta ingestibile. Provato su M1 pro.

---
+++ Indovinello/Versione1.py
@@ -0,0 +1,16 @@
+from itertools import permutations 
+  
+def testa(lista):
+    up=[1,3,5,7]
+    for i in up:
+        if lista[i] != (lista[i-1]+lista[i+1]):
+            return False
+    return True
+
+# Get all permutations of [1, 2, 3] 
+perm = permutations([1, 2, 3,4,5,6,7,8,9]) 
+
+# Print the obtained permutations 
+for i in list(perm): 
+    if testa(i):
+        print(f"Trovata soluzione: {i}") 
